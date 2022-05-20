88bada64125ca48289c4975050336b415588895c
Mary A Green <cyberangel9911@gmail.com>
2022-05-20 11:17:02 -0500

    added search bar

--- components/layout/Header.js
+++ components/layout/Header.js
@@ -1,4 +1,5 @@
-import { AppBar, Grid, Toolbar } from '@mui/material'
+import { AppBar, Grid, Toolbar, TextField } from '@mui/material'
+import { grey } from '@mui/material/colors'
 import React from 'react'
 import Logo from '../Logo'
 
@@ -7,13 +8,20 @@ export default function Header() {
     <AppBar position="static">
       <Toolbar>
         <Grid container spacing={2}>
-          <Grid item xs={2}>
+          <Grid item xs={1}>
             <div>
               <Logo />
             </div>
           </Grid>
-          <Grid div xs={6}>
-            <div>search</div>
+          <Grid div xs={7}>
+            <div>
+              <TextField
+                id="outlined-basic"
+                label="Search"
+                variant="outlined"
+                sx={{ backgroundColor: grey[50] }}
+              />
+            </div>
           </Grid>
           <Grid div xs={2}>
             <div>sign in</div>
