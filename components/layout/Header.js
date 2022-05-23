8b825799facb8d7198c6745d5ee7872f07aa49a4
Mary A Green <cyberangel9911@gmail.com>
2022-05-22 20:47:12 -0500

    Navigation Bar added

--- components/layout/Header.js
+++ components/layout/Header.js
@@ -1,5 +1,6 @@
-import { AppBar, Grid, Toolbar, TextField } from '@mui/material'
+import { AppBar, Grid, Toolbar, TextField, Box } from '@mui/material'
 import { grey } from '@mui/material/colors'
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart'
 import React from 'react'
 import Logo from '../Logo'
 
@@ -9,25 +10,35 @@ export default function Header() {
       <Toolbar>
         <Grid container spacing={2}>
           <Grid item xs={1}>
-            <div>
+            <Box sx={{ paddingTop: '10px' }}>
               <Logo />
-            </div>
+            </Box>
           </Grid>
-          <Grid div xs={7}>
-            <div>
+          <Grid item xs={9}>
+            <Box sx={{ paddingTop: '5px' }}>
               <TextField
                 id="outlined-basic"
                 label="Search"
                 variant="outlined"
-                sx={{ backgroundColor: grey[50] }}
+                sx={{
+                  backgroundColor: grey[50],
+                  width: '100%',
+                  borderRadius: '10px'
+                }}
+                size="small"
               />
+            </Box>
+          </Grid>
+          <Grid item xs={1}>
+            <div>
+              Hello, sign in <span style={{ fontWeight: 'bold' }}>Account</span>
             </div>
           </Grid>
-          <Grid div xs={2}>
-            <div>sign in</div>
-          </Grid>
-          <Grid div xs={2}>
-            <div>cart</div>
+          <Grid item xs={1}>
+            <Box sx={{ paddingTop: '5px' }}>
+              <AddShoppingCartIcon fontSize="large" />
+              <span style={{ fontWeight: 'bold' }}>Cart</span>
+            </Box>
           </Grid>
         </Grid>
       </Toolbar>
