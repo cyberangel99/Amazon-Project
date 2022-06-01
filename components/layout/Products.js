90a403329fb695e1cc67c1b03ffeddf45e5aaac5
Mary A Green <cyberangel9911@gmail.com>
2022-06-01 10:53:45 -0500

    Products.js
    added grid with cards

--- components/layout/Products.js
+++ components/layout/Products.js
@@ -1,5 +1,27 @@
+import { Card, Grid } from '@mui/material'
 import React from 'react'
 
 export default function Products() {
-  return <div>Products</div>
+  return (
+    <Grid container spacing={2}>
+      <Grid item xs={8}>
+        <Card>dfafd</Card>
+      </Grid>
+      <Grid item xs={4}>
+        <Card>dfafd</Card>
+      </Grid>
+      <Grid item xs={4}>
+        <Card>dfafd</Card>
+      </Grid>
+      <Grid item xs={4}>
+        <Card>dfafd</Card>
+      </Grid>
+      <Grid item xs={4}>
+        <Card>dfafd</Card>
+      </Grid>
+      <Grid item xs={12}>
+        <Card>dfafd</Card>
+      </Grid>
+    </Grid>
+  )
 }
