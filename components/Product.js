307a45d2840186e71badb44ca46f0a15bbb8171f
Mary A Green <cyberangel9911@gmail.com>
2022-06-05 12:13:43 -0500

    Api
    adding axios api

---
+++ components/Product.js
@@ -0,0 +1,10 @@
+import { Card, Grid } from '@mui/material'
+import React from 'react'
+
+export default function Product({ product }) {
+  return (
+    <Grid item xs={4}>
+      <Card>dfafd</Card>
+    </Grid>
+  )
+}
--- components/layout/Products.js
+++ components/layout/Products.js
@@ -1,30 +1,26 @@
-import { Card, Grid } from '@mui/material'
-import React from 'react'
+import { Grid } from '@mui/material'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import Product from '../Product'
 
 export default function Products() {
+  const [products, setProducts] = useState([])
+  const url = 'http://localhost:3000/api/products'
+
+  const getProducts = () => {
+    axios
+      .get(url)
+      .then((response) => {
+        console.log(response)
+      })
+      .catch((error) => console.error(`Error: ${error}`))
+  }
+  useEffect(() => {
+    getProducts()
+  }, [])
   return (
     <Grid container spacing={2}>
-      <Grid item xs={8}>
-        <Card>dfafd</Card>
-      </Grid>
-      <Grid item xs={4}>
-        <Card>dfafd</Card>
-      </Grid>
-      <Grid item xs={4}>
-        <Card>dfafd</Card>
-      </Grid>
-      <Grid item xs={4}>
-        <Card>dfafd</Card>
-      </Grid>
-      <Grid item xs={4}>
-        <Card>dfafd</Card>
-      </Grid>
-      <Grid item xs={8}>
-        <Card>dfafd</Card>
-      </Grid>
-      <Grid item xs={4}>
-        fdfdfd
-      </Grid>
+      <Product />
     </Grid>
   )
 }
