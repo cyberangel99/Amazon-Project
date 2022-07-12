8481dae33fa12494f893f95c9af29f162d7ab015
Mary A Green <cyberangel9911@gmail.com>
2022-07-11 20:37:46 -0500

    Products
    added images to product cards

--- components/Product.js
+++ components/Product.js
@@ -1,10 +1,19 @@
 import { Card, Grid } from '@mui/material'
 import React from 'react'
+import Image from 'next/image'
+import CardContent from '@mui/material/CardContent'
 
 export default function Product({ product }) {
   return (
     <Grid item xs={4}>
-      <Card>{product.name}</Card>
+      <Card>
+        {product.name}
+        <CardContent>
+          {product.price}
+          {product.rating}
+          <Image src={product.image} width="100%" height="100%" alt="product image" />
+        </CardContent>
+      </Card>
     </Grid>
   )
 }
